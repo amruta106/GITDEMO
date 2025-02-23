5e57b91fdb1bf248d0b385b422e7fc3907ad8fc3
amruta106 <bhosaleamruta286@gmail.com>
2025-02-23 20:43:49 +0530

    Update GreetingDemo.java

--- src/demoClass/GreetingDemo.java
+++ src/demoClass/GreetingDemo.java
@@ -3,6 +3,6 @@ package demoClass;
 public class GreetingDemo {
 public static void main(String[] args) {
 	System.out.println("hi");
-	System.out.println("Changes done by some other developer");
+	System.out.println("Changes done by me");
 }
 }
